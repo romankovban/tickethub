e12b7958792026fd964a9d8d6cc436496be7f563
Roman Kovban <roman.kovban@gmail.com>
2023-06-16 18:30:12 +0300

    Add first page

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,144 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { FC } from 'react';
 
-function App() {
+interface AppProps {}
+
+export const App: FC<AppProps> = ({}) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h3>
+        <strong>
+          <a href="index.html">Challenge Tickets</a>
+        </strong>
+      </h3>
+      <hr />
+      <h4>Select an event</h4>
+      <hr />
+      <div className="row">
+        <div className="col-sm-3 event-list">
+          {' '}
+          <a href="event.html">
+            {' '}
+            <img
+              src="https://cdn.boletius.com/images/1521202649764-test-pos-DA32F77A-C542-41B7-81C4-43091924255B.jpeg"
+              width="100%"
+            />
+          </a>
+        </div>
+        <div className="col-sm-3 event-list">
+          {' '}
+          <a href="event.html">
+            {' '}
+            <img
+              src="https://cdn.boletius.com/images/1520277905667-test-pos-All_access_640x640-min.jpg"
+              width="100%"
+            />
+          </a>
+        </div>
+        <div className="col-sm-3 event-list">
+          {' '}
+          <a href="event.html">
+            {' '}
+            <img
+              src="https://cdn.boletius.com/images/1519768708978-test-pos-rompiendo-cabezas-640-min.jpg"
+              width="100%"
+            />
+          </a>
+        </div>
+        <div className="col-sm-3 event-list">
+          {' '}
+          <a href="event.html">
+            {' '}
+            <img
+              src="https://cdn.boletius.com/images/1519139052461-test-pos-BANN.jpg"
+              width="100%"
+            />
+          </a>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-sm-3 event-list">
+          {' '}
+          <a href="event.html">
+            {' '}
+            <img
+              src="https://cdn.boletius.com/images/1521202649764-test-pos-DA32F77A-C542-41B7-81C4-43091924255B.jpeg"
+              width="100%"
+            />
+          </a>
+        </div>
+        <div className="col-sm-3 event-list">
+          {' '}
+          <a href="event.html">
+            {' '}
+            <img
+              src="https://cdn.boletius.com/images/1520277905667-test-pos-All_access_640x640-min.jpg"
+              width="100%"
+            />
+          </a>
+        </div>
+        <div className="col-sm-3 event-list">
+          {' '}
+          <a href="event.html">
+            {' '}
+            <img
+              src="https://cdn.boletius.com/images/1519768708978-test-pos-rompiendo-cabezas-640-min.jpg"
+              width="100%"
+            />
+          </a>
+        </div>
+        <div className="col-sm-3 event-list">
+          {' '}
+          <a href="event.html">
+            {' '}
+            <img
+              src="https://cdn.boletius.com/images/1519139052461-test-pos-BANN.jpg"
+              width="100%"
+            />
+          </a>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-sm-3 event-list">
+          {' '}
+          <a href="event.html">
+            {' '}
+            <img
+              src="https://cdn.boletius.com/images/1521202649764-test-pos-DA32F77A-C542-41B7-81C4-43091924255B.jpeg"
+              width="100%"
+            />
+          </a>
+        </div>
+        <div className="col-sm-3 event-list">
+          {' '}
+          <a href="event.html">
+            {' '}
+            <img
+              src="https://cdn.boletius.com/images/1520277905667-test-pos-All_access_640x640-min.jpg"
+              width="100%"
+            />
+          </a>
+        </div>
+        <div className="col-sm-3 event-list">
+          {' '}
+          <a href="event.html">
+            {' '}
+            <img
+              src="https://cdn.boletius.com/images/1519768708978-test-pos-rompiendo-cabezas-640-min.jpg"
+              width="100%"
+            />
+          </a>
+        </div>
+        <div className="col-sm-3 event-list">
+          {' '}
+          <a href="event.html">
+            {' '}
+            <img
+              src="https://cdn.boletius.com/images/1519139052461-test-pos-BANN.jpg"
+              width="100%"
+            />
+          </a>
+        </div>
+      </div>
     </div>
   );
-}
-
-export default App;
+};
