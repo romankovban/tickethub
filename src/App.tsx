bcdca75574f2361a82cc7130ddd05a976a841acf
Roman Kovban <roman.kovban@gmail.com>
2023-06-16 21:18:35 +0300

    Add single page event

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { HomePage } from './pages/home.page';
 import { Provider } from 'react-redux';
 import { store } from './store';
+import { EventPage } from './pages/event.page';
 
 interface AppProps {}
 
@@ -12,6 +13,7 @@ export const App: FC<AppProps> = ({}) => {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/event/:id" element={<EventPage />} />
         </Routes>
       </BrowserRouter>
     </Provider>
--- src/api/events.ts
+++ src/api/events.ts
@@ -1,6 +1,8 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import { GetEventResponseDto } from './dto/get-events.dto';
+import { GetEventResponseDto } from './dto/get-event-response.dto';
 import { config } from '../core/config';
+import { GetSingleEventResponseDto } from './dto/get-single-event-response.dto';
+import { GetSectorResponseDto } from './dto/get-sector-response.dto';
 
 export const eventsApi = createApi({
   reducerPath: 'eventsApi',
@@ -9,7 +11,18 @@ export const eventsApi = createApi({
     getEvents: builder.query<GetEventResponseDto, unknown>({
       query: () => '/api/event',
     }),
+    getSingleEvent: builder.query<GetSingleEventResponseDto, number>({
+      query: (id) => `/api/event/${id}`,
+    }),
+
+    getSectorByEvent: builder.query<GetSectorResponseDto, number>({
+      query: (id) => `/api/eventDate/${id}/sectors`,
+    }),
   }),
 });
 
-export const { useGetEventsQuery } = eventsApi;
+export const {
+  useGetEventsQuery,
+  useGetSingleEventQuery,
+  useLazyGetSectorByEventQuery,
+} = eventsApi;
